8c7e3115efcc389f17618e1251159853df712ca3
jaulf <howardotuya17@gmail.com>
2023-03-13 15:41:23 +0100

    minor change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/Image";
+import Image from "next/image";
 import logo2 from "../../public/logo2.svg";
 import menu from "../../public/menu.svg";
 import Dashboard from "../../public/Dashboard.png";
